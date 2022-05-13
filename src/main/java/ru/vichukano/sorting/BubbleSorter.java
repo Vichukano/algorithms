775a6d574f42beebec0a8e0ad35e26b99fef77a0
Vichukano <vichukano@gmail.com>
2022-05-14 01:07:05 +0300

    improve sorting

--- src/main/java/ru/vichukano/sorting/BubbleSorter.java
+++ src/main/java/ru/vichukano/sorting/BubbleSorter.java
@@ -5,23 +5,28 @@ import java.util.Objects;
 
 public class BubbleSorter {
 
-    public <E extends Comparable<E>> void sort(List<E> list) {
-        if (Objects.isNull(list)) {
-            throw new IllegalArgumentException("Can not sort null");
-        }
-        if (list.isEmpty() || list.size() == 1) {
-            return;
-        }
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.size() - i - 1; j++) {
-                var current = list.get(j);
-                var next = (E) list.get(j + 1);
-                if (current.compareTo(next) > 0) {
-                    list.set(j, next);
-                    list.set(j + 1, current);
-                }
-            }
-        }
+  public <E extends Comparable<E>> void sort(List<E> list) {
+    if (Objects.isNull(list)) {
+      throw new IllegalArgumentException("Can not sort null");
     }
+    if (list.isEmpty() || list.size() == 1) {
+      return;
+    }
+    boolean swapped = true;
+    for (int i = 0; i < list.size(); i++) {
+      if (!swapped) {
+        break;
+      }
+      for (int j = 0; j < list.size() - i - 1; j++) {
+        var current = list.get(j);
+        var next = (E) list.get(j + 1);
+        if (current.compareTo(next) > 0) {
+          list.set(j, next);
+          list.set(j + 1, current);
+          swapped = true;
+        }
+      }
+    }
+  }
 
 }
